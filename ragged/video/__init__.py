6f85bd3b2cc13c46afc05c41c1bc8ab3257c8f7e
Nikit Phadke <nikitph@gmail.com>
2025-06-28 16:12:21 +0530

    cleanup

--- ragged/video/__init__.py
+++ ragged/video/__init__.py
@@ -1,10 +1,12 @@
 from .encoder import VectorMP4Encoder
 from .decoder import VectorMP4Decoder
+from .CDNDecoder import CDNVectorMP4Decoder
 from .config import get_default_config, get_codec_parameters
 
 __all__ = [
     "VectorMP4Encoder",
     "VectorMP4Decoder",
+    "CDNVectorMP4Decoder",
     "get_default_config",
     "get_codec_parameters",
 ]
